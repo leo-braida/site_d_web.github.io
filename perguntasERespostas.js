a52be28805acf014fafda746c9553826046c797a
leo-braida <153570963+leo-braida@users.noreply.github.com>
2023-12-17 14:19:19 -0300

    Add files via upload

--- perguntasERespostas.js
+++ perguntasERespostas.js
@@ -2,13 +2,17 @@ function verifica(){
     let certo = 0;
     let quant = document.querySelector('span[data-quant-perguntas]').dataset.quantPerguntas;
     let i;
-    for(i= 0; i < quant; i++){
-        let radio = document.querySelector(`input[name="p${i}"][checked]`);
+    let inputs = document.querySelectorAll("input[name='p']");
 
+    for(i = 0; i<quant; i++){
+        let nome = "p" + i;
+        const radio = document.querySelector(`input[name="${nome}"]`);
+        if(radio.checked){
             if(radio.value === "certa"){
                 certo++;
-                alert("teste");
             }
+        }
     }
-    window.alert(certo);
+
+    //Agora falta ter o lugar de enviar o quiz no html para eu poder linkar com o valor de certo
 }
