2df88fa7b697a39f42e7544ac04ae5dd65882f7f
leo-braida <153570963+leo-braida@users.noreply.github.com>
2023-12-18 16:19:07 -0300

    Add files via upload

--- perguntasERespostas.js
+++ perguntasERespostas.js
@@ -1,18 +1,44 @@
-function verifica(){
-    let certo = 0;
-    let quant = document.querySelector('span[data-quant-perguntas]').dataset.quantPerguntas;
-    let i;
-    let inputs = document.querySelectorAll("input[name='p']");
+var certo;
 
-    for(i = 0; i<quant; i++){
+function verifica(){
+    certo = 0;
+    let quant = document.querySelector('span[data-quant-perguntas]').dataset.quantPerguntas;
+    for(let i = 0; i<quant; i++){
         let nome = "p" + i;
-        const radio = document.querySelector(`input[name="${nome}"]`);
-        if(radio.checked){
-            if(radio.value === "certa"){
-                certo++;
+        let radios = document.querySelectorAll(`input[name="${nome}"]`);
+        for(let radio of radios){
+            if(radio.checked){
+                if(radio.value === "certa"){
+                    certo++;
+                }
+            }
+        }
+    }
+    //Agora falta ter o lugar de enviar o quiz no html para eu poder linkar com o valor de certo
+}
+
+
+/* 
+temos duas opções:
+ -> Colocar uma classe em todas as corretas
+ -> Colocar um laço sempre que quiser pegar as corretas (pode gerar um delay para aparecer as cores)
+*/
+
+function correcao(){
+    let quant = document.querySelector('span[data-quant-perguntas]').dataset.quantPerguntas;
+    for(let i = 0; i<quant; i++){
+        let nome = "p" + i;
+        let radios = document.querySelectorAll(`input[name="${nome}"]`);
+        for(let radio of radios){
+            if(radio.value == "certa"){
+                radio.parentElement.style.backgroundColor = "#34FF37"; //estou pegando o input, mas preciso pegar toda label ao redor do input
+            }
+            else{
+                radio.parentElement.style.backgroundColor = "#E71D1D";
             }
         }
     }
 
-    //Agora falta ter o lugar de enviar o quiz no html para eu poder linkar com o valor de certo
+    const labels = document.querySelectorAll("label");
+    document.getElementById("pontuacao").innerHTML = `Pontuação: ${certo}/${quant}`;
 }
