5b1c48215090a63699bd1659ffee351fca3dedcb
leo-braida <153570963+leo-braida@users.noreply.github.com>
2023-12-22 09:23:39 -0300

    Add files via upload

---
+++ emoji.js
@@ -0,0 +1,61 @@
+var dados = [
+    ["LEAGUE OF LEGENDS", "👬", "⚔️", "🏰", "🧚"],
+    ["THE LEGENDS OF ZELDA", "🐎", "🧝", "🎭", "🏹"],
+    ["WE HAPPY FEW", "😁", "😞", "🎭", "💊"],
+    ["MINECRAFT", "⛏️", "🌎", "🏕️", "🧟"],
+    ["VALORANT", "👬", "🧚", "🔫", "💣"],
+    ["ROCKET LEAGUE", "🚀", "🥅", "⚽", "🚗"],
+    ["FIFA", "👬", "🏃🏻", "🥅", "⚽"],
+    ["JOGO DO TIGRINHO", "🎰", "💸", "🐅", "🐯"],
+    ["THE SIMS", "🧑", "🏠", "👶", "💼"],
+    ["BLOONS TD 6", "🎯", "🍌", "🐵", "🎈"],
+    ["BORDERLANDS", "🪐", "👾", "🔫", "👽"],
+    ["GTA 5", "🔫", "🚗", "💸", "👮"],
+    ["GOD OF WAR", "🏛️", "👶", "🛡️", "🪓"],
+    ["SPIDER-MAN", "🐙", "🦸‍♂️", "🕷️", "🕸️"],
+    ["POKEMON", "👜", "⚡", "🔮", "👾"],
+    ["RED DEAD REDEMPTION", "🌵", "🔫", "🐎", "🤠"],
+    ["CS", "👬", "💥", "💣", "🔫"],
+    ["AMONG US", "🎭", "🛸", "🤥", "👽"],
+    ["FORTNITE", "⛏️", "🏗️", "🔫", "🚌"],
+    ["FORZA", "🛣️", "🏁", "💨", "🚗"],
+    ["PAC-MAN", "🍒", "👻", "🟡", ":V"],
+    ["FIVE NIGHTS AT FREDDY'S", "🤖", "📷", "🦊", "🐻"],
+    ["THE LAST OF US", "🍄", "🔫", "🧬", "🦠"],
+    ["MARIO", "🐲", "👸", "⭐", "🍄"]
+  ];
+  
+  var jogo;
+  var emojis = [];
+
+  function selecionaJogo(){
+    let id = Math.floor(Math.random()*24);
+    jogo = dados[id][0];
+    emojis = [dados[id][1], dados[id][2], dados[id][3], dados[id][4]];
+  }
+  
+
+  var p = 0;
+  function verifica(){
+    let resposta = document.querySelector(`input[name="resposta"]`).value.toUpperCase();
+    
+    if(resposta != jogo){
+      if(p<4){
+        p++;
+        let emoji = emojis[p-1];
+        document.getElementById("emojis").innerHTML += emoji;
+      }
+    }
+    else{
+      document.getElementById("emojis").innerHTML = emojis[0] + emojis[1] + emojis[2] + emojis[3];
+      document.getElementById("caixa").style.backgroundColor = "rgb(109, 252, 145)";
+      document.getElementById("resultado").innerHTML = "PARABÉNS! VOCÊ ACERTOU!";
+    }
+  }
+  
+  function verificaEnter(event){
+    if(event.key === "Enter"){
+      verifica();
+    }
+  }
+  
