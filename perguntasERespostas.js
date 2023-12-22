bbd7b3fb651485694b079421bef7c7473ecb63cb
leo-braida <153570963+leo-braida@users.noreply.github.com>
2023-12-22 02:05:00 -0300

    Add files via upload

--- perguntasERespostas.js
+++ perguntasERespostas.js
@@ -12,6 +12,16 @@ function verifica(){
                 if(radio.value === "certa"){
                     certo++;
                 }
+                    let labels = radio.labels;
+                    for (var j = 0; j < labels.length; j++) {
+                    labels[j].style.backgroundColor = 'blue';
+                    }
+            }
+            if(!radio.checked){
+                let labels = radio.labels;
+                    for (var j = 0; j < labels.length; j++) {
+                    labels[j].style.backgroundColor = 'rgb(134, 166, 255)';
+                    }
             }
         }
     }
@@ -23,7 +33,7 @@ function correcao(){
         let radios = document.querySelectorAll(`input[name="${nome}"]`);
         for(let radio of radios){
             if(radio.value == "certa"){
-                radio.parentElement.style.backgroundColor = "#34FF37"; //estou pegando o input, mas preciso pegar toda label ao redor do input
+                radio.parentElement.style.backgroundColor = "#34FF37";
             }
             else{
                 radio.parentElement.style.backgroundColor = "#E71D1D";
